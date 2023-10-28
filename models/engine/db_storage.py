56937670b09ef7c480e5b3c1556993150c412bcd
John Mwadime <johnniekips@gmail.com>
2023-10-28 17:42:33 +0300

    Update db_storage.py

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,35 +1,23 @@
 #!/usr/bin/python3
-from models.amenity import Amenity
 from models.base_model import Base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import (sessionmaker, scoped_session)
+from os import getenv
+from models.user import User
+from models.amenity import Amenity
 from models.city import City
 from models.place import Place
 from models.review import Review
 from models.state import State
-from models.user import User
-from os import getenv
-from sqlalchemy import (create_engine, func)
-from sqlalchemy.orm import (sessionmaker, scoped_session)
 """
-This is the db_storage module.
-This module deals with storing and retrieving data from a mysql database.
-This module contains one class DBStorage.
+This is the db_storage module
 """
 
 
 class DBStorage:
-    """
-    class DBStorage
-    Save and retrieve data from a MySQL database using sqlAlchemy ORM
-
-    **class attributes**
-       __engine: private, sqlAlchemy engine
-       __session: private, MySQL session
-
-    instance attributes:
-       __models_available: private, dictionary of <string> <class>
-    """
     __engine = None
     __session = None
+    __Session = None
 
     def __init__(self):
         """
@@ -40,7 +28,6 @@ class DBStorage:
             getenv('HBNB_MYSQL_PWD'),
             getenv('HBNB_MYSQL_HOST'),
             getenv('HBNB_MYSQL_DB')))
-        # adding, echo=True) shows SQL statements
         self.__models_available = {"User": User,
                                    "Amenity": Amenity, "City": City,
                                    "Place": Place, "Review": Review,
@@ -48,16 +35,21 @@ class DBStorage:
         if getenv('HBNB_MYSQL_ENV', 'not') == 'test':
             Base.metadata.drop_all(self.__engine)
 
+    @property
+    def available_classes(self):
+        """
+        Returns Available classes
+        """
+        return (self.__models_available)
+
     def all(self, cls=None):
         """
         returns a dictionary of all the class objects
         """
         orm_objects = {}
         if cls:
-            if cls in self.__models_available:
-                for k in self.__session.query(
-                        self.__models_available.get(cls)):
-                    orm_objects[k.__dict__['id']] = k
+            for k in self.__session.query(self.__models_available[cls]):
+                orm_objects[k.__dict__['id']] = k
         else:
             for i in self.__models_available.values():
                 j = self.__session.query(i).all()
@@ -72,19 +64,34 @@ class DBStorage:
         """
         self.__session.add(obj)
 
+    def get(self, cls, id):
+        """
+        gets an object of a certain kind of class
+        """
+        if cls not in self.__models_available.keys():
+            return (None)
+        for class_instance in self.__session.query(
+                self.__models_available[cls]):
+            if class_instance.__dict__['id'] == id:
+                return (class_instance)
+        print("ERROR: Instance ID does not exist")
+        return (None)
+
+    def count(self, cls=None):
+        """
+        counts the number of instances of a class (cls)
+        """
+        if cls is not None:
+            if self.__models_available.get(cls) is not None:
+                return(len(self.all(cls)))
+        else:
+            return(len(self.all()))
+
     def save(self):
         """
         saves the objects fom the current session
         """
         self.__session.commit()
-
-    def delete(self, obj=None):
-        """
-        deletes an object from the current session
-        """
-        if obj is not None:
-            self.__session.delete(obj)
-            self.__session.commit()
 
     def reload(self):
         """
@@ -92,47 +99,17 @@ class DBStorage:
         be in the init method
         """
         Base.metadata.create_all(self.__engine)
-        self.__session = scoped_session(sessionmaker(bind=self.__engine,
-                                                     expire_on_commit=False))
+        self.__session = scoped_session(sessionmaker(bind=self.__engine, expire_on_commit=False))
+
+    def delete(self, obj=None):
+        """
+        deletes an object from the current session
+        """
+        if obj is not None:
+            self.__session.delete(obj)
 
     def close(self):
         """
         close a session
         """
         self.__session.remove()
-
-    def get(self, cls, id_):
-        """
-        Retrieve one object
-
-        Arguments:
-            cls: string representing a class name
-            id_: string representing the object id, primary key
-
-        Return:
-           object of cls and id passed in argument or None
-        """
-        if (cls not in self.__models_available) or (id_ is None):
-            return None
-        return self.__session.query(
-                self.__models_available[cls]).get(id_)
-
-    def count(self, cls=None):
-        """
-        Number of objects in a certain class
-
-        Arguments:
-            cls: optional, string representing a class name (default None)
-
-        Return:
-            number of objects in that class or in total
-            -1 if the argument is not valid
-        """
-        if cls is None:
-            total = 0
-            for v in self.__models_available.values():
-                total += self.__session.query(v).count()
-            return total
-        if cls in self.__models_available.keys():
-            return self.__session.query(self.__models_available[cls]).count()
-        return -1
