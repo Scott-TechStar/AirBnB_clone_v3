c44e634104c03e10b602ce3349de375258ac9c5b
John Mwadime <johnniekips@gmail.com>
2023-10-28 19:33:52 +0300

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,33 +1,40 @@
 #!/usr/bin/python3
-"""App API version 1 using flask"""
-from flask import Flask
+"""
+API for AirBnB_clone_v3
+"""
+
+import os
+from flask import Flask, jsonify, Response
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
-from os import getenv
-from flask import jsonify
-
-# Create a variable app, instance of Flask.
 app = Flask(__name__)
-
-# Register blueprint app_views
 app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
-# Function to be called when app is popped.
 @app.teardown_appcontext
-def teardown_appcontext(exception):
-    """Close storage"""
+def teardown(self):
+    """ handles teardown """
     storage.close()
 
 
+@app.errorhandler(404)
 def page_not_found(e):
-    return jsonify({'error': "Not found"}), 404
+    """ handles 404 errors """
+    status = {"error": "Not found"}
+    return jsonify(status), 404
 
 
-if __name__ == "__main__":
-    host = getenv('HBNB_API_HOST', '0.0.0.0')
-    port = getenv('HBNB_API_PORT', '5000')
+if __name__ == '__main__':
+    try:
+        host = os.environ.get('HBNB_API_HOST')
+    except:
+        host = '0.0.0.0'
 
-    app.register_error_handler(404, page_not_found)
+    try:
+        port = os.environ.get('HBNB_API_PORT')
+    except:
+        port = '5000'
 
-    app.run(host=host, port=port, threaded=True)
+    app.run(host=host, port=port)
